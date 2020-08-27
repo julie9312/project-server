2b46a2be4475aba03cd28d4452bb5fb639bef25c
julie9312 <67768114+julie9312@users.noreply.github.com>
2020-08-27 17:28:04 +0900

    add1

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 const users = require("./routes/users");
-const memo = require("./routes/memo");
+const memo = require("./routes/memos");
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 app.use(express.json());
-app.use(fileupload());
 
 app.use("/api/v1/users", users);
-app.use("/api/v1/memo", memo);
+app.use("/api/v1/memos", memo);
 
 const PORT = process.env.PORT || 5900;
 app.listen(PORT, console.log("서버 실행됨"));
